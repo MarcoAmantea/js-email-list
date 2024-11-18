80ddae054c3745b4a9aa2a15f79aea6d15ae70b0
MarcoAmantea <marcoamantea18@gmail.com>
2024-11-18 16:41:00 +0100

    feat: btn Bonus

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const mailElem = document.getElementById("mail");
-const refreshBtn = document.getElementById("refresh")
+const refreshBtn = document.getElementById("refresh");
 
-function randomMail(numEmail){
-    for (let i = 0; i < numEmail; i++) {
+function randomMail(){
+    for (let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-            mailElem.innerHTML += `<a href=""><li>${resp.data.response}</li></a>`    
-        })
+            mailElem.innerHTML += `<a href=""><li>${resp.data.response}</li></a>`;
+            })   
+        }
     };
-}
-randomMail(10)
+randomMail();
+refreshBtn.addEventListener("click", randomMail);
